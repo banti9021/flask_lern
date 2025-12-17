f95d9d84185950a9104c062bc6ea8d7e74259e8b
banti9021 <nbanti943@gmail.com>
2025-12-17 16:09:29 +0530

    updated code

--- app1.py
+++ app1.py
@@ -5,7 +5,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("ho.html")
+    return render_template("login.html")
+@app.route("/submit",methods=["POST"])
+def submit():
+    username=request.form.get("username")
+    password=request.form.get("username")
+    if username=="sager123"and password=="pass":
+         return render_template('welcome html',name=username)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
